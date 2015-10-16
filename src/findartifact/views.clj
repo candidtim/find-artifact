e751b5995821d545acf75b4d840f2ad0080e972a
Timur Rubeko <trubeko@kyriba.com>
2015-10-16 19:55:12 +0200

    Factorized search form HTML snippet

--- src/findartifact/views.clj
+++ src/findartifact/views.clj
@@ -6,12 +6,17 @@
   [:#title] (enlive/content title)
   [:div#main] (enlive/substitute content))
 
+(enlive/defsnippet search-form "templates/searchform.html" [:form#search] [autofocus query]
+  [:input#query] (if autofocus (enlive/set-attr :autofocus "true") identity)
+  [:input#query] (if query (enlive/set-attr :value query) identity))
+
 
 ; Index
-(enlive/defsnippet index-content "templates/index.html" [:div#main] [])
+(enlive/defsnippet index-content "templates/index.html" [:div#main] [form]
+  [:div#search] (enlive/content form))
 
 (defn index []
-  (base "Find Artifact - Consise UI for wide Maven artifact search" (index-content)))
+  (base "Find Artifact - Consise UI for wide Maven artifact search" (index-content (search-form true nil))))
 
 
 ; About
@@ -22,8 +27,8 @@
 
 
 ; Results list
-(enlive/defsnippet results-content "templates/results.html" [:div#main] [query {results :docs :as query-result}]
-  [:input#query] (enlive/set-attr :value query)
+(enlive/defsnippet results-content "templates/results.html" [:div#main] [form {results :docs :as query-result}]
+  [:div#search] (enlive/content form)
   [:span#results-count] (enlive/content (str (:numFound query-result)))
   [:div.list [:div.artifact]] (enlive/clone-for [artifact results]
     [:a.name] (enlive/content (:id artifact))
@@ -33,4 +38,4 @@
     [:span.location] (enlive/content (:repositoryId artifact))))
 
 (defn results [query query-result]
-  (base (format "%s - Find Artifact" query) (results-content query query-result)))
+  (base (format "%s - Find Artifact" query) (results-content (search-form false query) query-result)))
