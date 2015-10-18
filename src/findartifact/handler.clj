0bbab74ab51234f24b22ce9ac503cb07b0a6bb47
Timur Rubeko <trubeko@kyriba.com>
2015-10-18 17:14:27 +0200

    Added artifact details page

--- src/findartifact/handler.clj
+++ src/findartifact/handler.clj
@@ -7,14 +7,25 @@
             [findartifact.views :as views]))
 
 
+(def maven-central-url "http://search.maven.org/solrsearch/select")
+
 (defn query-maven-central [query from rows]
-  (get-in (client/get "http://search.maven.org/solrsearch/select"
-            {:query-params {"q" query  "start" from "rows" rows "wt" "json"} :as :json})
+  (get-in (client/get maven-central-url
+            {:query-params {"q" query  "start" from "rows" rows "wt" "json"}
+             :as :json})
           [:body :response]))
+
+(defn get-from-maven-central [g a]
+  (first (get-in (client/get maven-central-url
+                   {:query-params {"q" (format "g:\"%s\" AND a:\"%s\"" g a) "rows" 1 "core" "gav" "wt" "json"}
+                    :as :json})
+                 [:body :response :docs])))
+
 
 (defroutes app-routes
   (GET "/" [] (views/index))
-  (GET "/search" [q] (views/results q (query-maven-central q 0 20)))
+  (GET "/search" [q] (views/results q (query-maven-central q 0 3000)))
+  (GET "/artifact" [g a] (views/artifact (get-from-maven-central g a)))
   (GET "/about" [] (views/about))
   (route/not-found "Not Found"))
 
--- src/findartifact/views.clj
+++ src/findartifact/views.clj
@@ -10,6 +10,17 @@
   [:input#query] (if autofocus (enlive/set-attr :autofocus "true") identity)
   [:input#query] (if query (enlive/set-attr :value query) identity))
 
+(def build-tools [
+  [:maven "Maven" "<dependency>\n  <groupId>%s</groupId>\n  <artifactId>%s</artifactId>\n  <version>%s</version>\n</dependency>"]
+  [:gradle "Gradle" "'%s:%s:%s'"]
+  [:sbt "SBT" "libraryDependencies += \"%s\" %% \"%s\" %% \"%s\""]
+  [:ivy "Ivy" "<dependency org=\"%s\" name=\"%s\" rev=\"%s\"/>"]
+  [:buildr "Buildr" "'%s:%s:jar:%s'"]
+  [:grape "Grape" "@Grab(group='%s', module='%s', version='%s')"]
+  [:lein "Leiningen" "[%s/%s \"%s\"]"]])
+
+(def default-build-tool :gradle)
+
 
 ; Index
 (enlive/defsnippet index-content "templates/index.html" [:div#main] [form]
@@ -32,10 +43,41 @@
   [:span#results-count] (enlive/content (str (:numFound query-result)))
   [:div.list [:div.artifact]] (enlive/clone-for [artifact results]
     [:a.name] (enlive/content (:id artifact))
-    [:a.name] (enlive/set-attr :href (format "artifact?c=%s" (:id artifact)))
+    [:a.name] (enlive/set-attr :href (format "artifact?g=%s&a=%s" (:g artifact) (:a artifact)))
     [:span.latest-version] (enlive/content (str (:latestVersion artifact)))
     [:span.version-count] (enlive/content (str (dec (:versionCount artifact))))
     [:span.location] (enlive/content (:repositoryId artifact))))
 
 (defn results [query query-result]
   (base (format "%s - Find Artifact" query) (results-content (search-form false query) query-result)))
+
+
+; Artifact details
+
+(defn get-classifiers [all-artifacts]
+  (map #(.substring % 1 (.indexOf % "."))
+  (filter #(.startsWith % "-")
+  all-artifacts)))
+
+(enlive/defsnippet artifact-content "templates/artifact.html" [:div#main] [form {group :g artifact :a version :v all-artifacts :ec}]
+  [:div#search] (enlive/content form)
+  ; tabs titles
+  [:ul.nav-tabs [:li.tab-item]] (enlive/clone-for [[tool tool-name _] build-tools]
+    [:a.tab-title] (enlive/content tool-name)
+    [:a.tab-title] (enlive/set-attr :href (format "#tab-%s" tool-name))
+    [:li.tab-item] (if (= tool default-build-tool) (enlive/add-class "active") identity))
+  ; tabs contents
+  [:div.tab-content [:div.tab-pane]] (enlive/clone-for [[tool tool-name tool-fmt] build-tools]
+    [:pre.dependency-id] (enlive/content (format tool-fmt group artifact version))
+    [:pre.dependency-id] (enlive/set-attr :id (format "dependency-%s" tool-name))
+    [:div.add-info] (if (empty? (get-classifiers all-artifacts)) nil identity)
+    [:span.classifiers] (enlive/content (clojure.string/join ", " (get-classifiers all-artifacts)))
+    [:div.btn-copy] (enlive/set-attr :data-clipboard-target (format "dependency-%s" tool-name))
+    [:div.btn-set-default] (enlive/set-attr :title (format "Make %s default" tool-name))
+    [:div.btn-set-default] (enlive/set-attr :onclick (format "setDefaultBuildTool('%s')" tool-name))
+    [:span.btn-title] (enlive/content tool-name)
+    [:div.tab-pane] (if (= tool default-build-tool) (enlive/add-class "active") identity)
+    [:div.tab-pane] (enlive/set-attr :id (format "tab-%s" tool-name))))
+
+(defn artifact [{group :g artifact-name :a :as artifact}]
+  (base "Artifact - Find Artifact" (artifact-content (search-form false (format "%s:%s" group artifact-name)) artifact)))
