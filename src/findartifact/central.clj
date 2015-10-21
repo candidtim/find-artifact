1204753add96c2f638e4df11990494e7137687c8
Timur Rubeko <trubeko@kyriba.com>
2015-10-21 22:08:32 +0200

    Moved query functions to a dedicated module - preparing to add more queries

---
+++ src/findartifact/central.clj
@@ -0,0 +1,17 @@
+(ns findartifact.central
+  (:require [clj-http.client :as client]))
+
+
+(def maven-central-url "http://search.maven.org/solrsearch/select")
+
+(defn query [query from rows]
+  (get-in (client/get maven-central-url
+            {:query-params {"q" query  "start" from "rows" rows "wt" "json"}
+             :as :json})
+          [:body :response]))
+
+(defn get-artifact [g a]
+  (first (get-in (client/get maven-central-url
+                   {:query-params {"q" (format "g:\"%s\" AND a:\"%s\"" g a) "rows" 1 "core" "gav" "wt" "json"}
+                    :as :json})
+                 [:body :response :docs])))
